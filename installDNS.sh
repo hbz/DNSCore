c406673cfe49bf3c56462d549c45c0245ba2ce3e
Jan Schnasse <schnasse@gmx.de>
2019-03-13 13:34:47 +0100

    refact 12 - remove commented code and sensless checks
    Former-commit-id: e236534db264c649141979a8de047746dad651a0

--- installDNS.sh
+++ installDNS.sh
@@ -82,15 +82,14 @@ function installEPEL(){
 	fi
 }
 
-function installDNS(){
-	if [ "1" -eq "1" ]; then  # yum kennt sonst kein HandBrake und ffmpeg
-		cp $SCPATH/data/dns-7-repo.tgz /etc/yum.repos.d
-		cd /etc/yum.repos.d; 
-		tar -xzvf dns-7-repo.tgz
-		rpm --import $SCPATH/data/RPM-GPG-KEY-nux
-		rm -f dns-7-repo.tgz
-		yum update
-	fi 
+function installDNS(){	
+	cp $SCPATH/data/dns-7-repo.tgz /etc/yum.repos.d
+	cd /etc/yum.repos.d; 
+	tar -xzvf dns-7-repo.tgz
+	rpm --import $SCPATH/data/RPM-GPG-KEY-nux
+	rm -f dns-7-repo.tgz
+	yum update
+
 }
 
 function setEnvironmentVariables(){
@@ -107,35 +106,24 @@ function setEnvironmentVariables(){
 }
 
 function setUpUsers(){
-	### User anlegen
 	groupadd -g 401 irods
 	groupadd -g 402 postgres   
 	groupadd -f -g 403 tomcat
-	#groupadd -g 404 isupport 
 	useradd -c "irods user" -d /home/irods -s /bin/bash -g 401 -u 401 irods
 	useradd -c "postgres db user" -d /home/postgres -s /bin/bash -g 402 -u 402 postgres
 	useradd -c "tomcat user" -d /home/tomcat -s /bin/bash -g 403 -G 401 -u 403 tomcat
-	#useradd -c "irods support user" -d /home/isupport -s /bin/bash -g 404 -G 401 -u 404 isupport
 	cat /etc/group | grep ":40"
 	cat /etc/passwd | grep ":40"
 	groupadd -g 12348 developer
 	usermod -a -G developer irods
-	####script e
 }
 
 function installJava(){
 	java -version
 	ls -l /usr/bin/java
-	### java pruefen
 	if [ ! -d /usr/java/jdk1.8.0_181-amd64 ] ; then
-	   #rm -f /usr/bin/java
 	   echo "JDK 1.8.0.181 wird installiert."
 	   mkdir /usr/java
-	   #cp $SCPATH/../ship/jdk-6u45-linux-x64.bin /usr/java
-	   #cd /usr/java; chmod a+x *bin; ./jdk-6u45-linux-x64.bin 
-	   #rm -f jdk-6u45-linux-x64.bin
-	   #ln -s /usr/java/jdk1.6.0_45 /usr/java/latest
-	   #ln -s /usr/java/latest /usr/java/default
 	   yum localinstall -y $SCPATH/data/jdk-8u181-linux-x64.rpm
 	fi
 
@@ -149,7 +137,6 @@ function installJava(){
 	echo "export JAVA_HOME=/usr/java/jdk1.8.0_181-amd64" >> ~irods/.bashrc
 	echo "export JAVA_HOME=/usr/java/jdk1.8.0_181-amd64" >> ~tomcat/.bashrc
 	echo "export JAVA_HOME=/usr/java/jdk1.8.0_181-amd64" >> /etc/profile.d/dns.sh
-
 	java -version
 	ANSWER=$(java -version 2>&1 | head -n 1 | cut -f 3 -d " " )
 	echo "java ANSWER: $ANSWER"
@@ -219,34 +206,31 @@ function createPostgresDBs(){
 }
 
 function installFedora(){
-	if [ "1" == "1" ] ; then # dieser Bereich sollte spaeter durch das fedora installskript ersetzt werden
-		echo "fedora install"
-		cd $SCPATH/data/ 
-		tar -xzf $SCPATH/data/FED-DB-20180517.dump.tgz
-		psql -d FED -U fed_usr < ./FED-DB-20180517.dump
-		rm -f $SCPATH/data/FED-DB-20180517.dump
-		sleep 1
-		echo "fedora unpack tar"
-		tar -xzf $SCPATH/data/fedora-files.tgz
-		if [ -d /ci/fedora ] ; then
-			rm -rf /ci/fedora
-		fi
-		mv fedora /ci/
-		chown -R irods:developer /ci/fedora
-		rm -rf fedora
-	
-		systemctl stop tomcat
-		sed -i "s/unpackWARs=\"true\" autoDeploy=\"true\"/unpackWARs=\"true\" autoDeploy=\"false\"/g" /usr/share/tomcat/conf/server.xml 
-		cp $SCPATH/data/fedoraTomcatConf.xml /usr/share/tomcat/conf/Catalina/localhost/fedora.xml	
-		cd /usr/share/tomcat/webapps/
-		if [ -d /usr/share/tomcat/webapps/fedora ] ; then 
-			rm -rf /usr/share/tomcat/webapps/fedora;
-		fi
-		tar -xzf $SCPATH/data/fedoraNoWar.tgz
-		rm -f fedora.war
-		systemctl enable tomcat
-		systemctl start tomcat
+	echo "fedora install"
+	cd $SCPATH/data/ 
+	tar -xzf $SCPATH/data/FED-DB-20180517.dump.tgz
+	psql -d FED -U fed_usr < ./FED-DB-20180517.dump
+	rm -f $SCPATH/data/FED-DB-20180517.dump
+	sleep 1
+	echo "fedora unpack tar"
+	tar -xzf $SCPATH/data/fedora-files.tgz
+	if [ -d /ci/fedora ] ; then
+		rm -rf /ci/fedora
 	fi
+	mv fedora /ci/
+	chown -R irods:developer /ci/fedora
+	rm -rf fedora
+	systemctl stop tomcat
+	sed -i "s/unpackWARs=\"true\" autoDeploy=\"true\"/unpackWARs=\"true\" autoDeploy=\"false\"/g" /usr/share/tomcat/conf/server.xml 
+	cp $SCPATH/data/fedoraTomcatConf.xml /usr/share/tomcat/conf/Catalina/localhost/fedora.xml	
+	cd /usr/share/tomcat/webapps/
+	if [ -d /usr/share/tomcat/webapps/fedora ] ; then 
+		rm -rf /usr/share/tomcat/webapps/fedora;
+	fi
+	tar -xzf $SCPATH/data/fedoraNoWar.tgz
+	rm -f fedora.war
+	systemctl enable tomcat
+	systemctl start tomcat
 	sleep 1
 }
 
@@ -279,7 +263,6 @@ function installIRODS(){
 	systemctl stop irods 
 }
 function configureIRODS(){
-	#irsed -i "s/testpassword/$RODSPASS/g"  /etc/irods/database_config.json
 	sed -i "s/SHA256/MD5/g" /etc/irods/server_config.json
 
 	if [ -f /etc/init.d/irods ]; then
@@ -290,45 +273,24 @@ function configureIRODS(){
 	cp $SCPATH/data/irodsC7 /etc/systemd/system/irods.service
 	systemctl enable irods
 	systemctl start irods
-		
-		
-		
 	ZONES="12345"	
 	echo "Zone $ZONES"
 	ZONEKEY="dns"$ZONES"dns"$ZONES"dns"$ZONES"dns"$ZONES
 	echo "Zonenkey $ZONEKEY"
 	echo "Default-Dir $CACHEDIR"
-
 	printf "irods\nirods\n$ZONENAME\n1247\n\n\n$CACHEDIR\ndns$ZONES\n$ZONEKEY\n1248\ndnszone-dnszone-dnszone-dnszone-\noff\nrods\n$RODSPASS\nyes\nlocalhost\n\n\n\n$ICATPASS\nyes\n" | /var/lib/irods/packaging/setup_irods.sh
-	
 	sleep 3
-
 	systemctl start irods 
 	sleep 1
-	#service irods stop
-	#systemctl stop irods
-	#sleep 1
-	#sed -i 's!"icat_host": null!"icat_host": "$(hostname)"!g' /etc/irods/server_config.json
-	#sed -i 's!"icat_host": null!"icat_host": "$(hostname)"!g' /etc/irods/server_config.json
-
-	#sed -i 's!\"icat_host\": \"localhost\"!\"icat_host\": \""$(hostname)"\"!g' /etc/irods/server_config.json
-	#service irods start
-	#systemctl start irods
 	sed -i 's!\"default_dir_mode\": \"0750\"!\"default_dir_mode\": \"0775\"!g' /etc/irods/server_config.json
 	sed -i 's!\"default_file_mode\": \"0600\"!\"default_file_mode\": \"0664\"!g' /etc/irods/server_config.json
-
 	iusrcmd "printf 'y\n' | /usr/bin/iadmin modresc demoResc name $CACHERESC"
-	#service irods stop
 	systemctl stop irods
 	sleep 1
-
-
 	sed -i 's/SHA256/MD5/g' ~irods/.irods/irods_environment.json
 	sed -i "s/demoResc/$CACHERESC/g" /etc/irods/server_config.json
 	sed -i "s/demoResc/$CACHERESC/g" ~irods/.irods/irods_environment.json
 	sed -i "s/demoResc/$CACHERESC/g" /etc/irods/core.re
-
-	#begin
 	service irods start	
 }
 
@@ -396,9 +358,10 @@ function createStorageAreas(){
 	chown -R irods:developer /ci/DNSCore
 	chown -R irods:developer /ci/storage
 }
+
 function linkPythonToCI(){
-mkdir -p /ci/python/
-ln -s /usr/bin/python2.7 /ci/python/python
+	mkdir -p /ci/python/
+	ln -s /usr/bin/python2.7 /ci/python/python
 }
 
 ZONENAME="ci"
